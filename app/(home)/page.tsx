9d047607a40ac19b1f49a912c796cc97ae9fe8f7
brain-ambani <brainambani1@gmail.com>
2024-07-08 08:05:42 +0300

    updated the save to db logic

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,9 @@
 import Navbar from "@/app/(home)/components/navbar";
 import Hero from "./components/hero";
+import { checkUser } from "@/lib/checkUser";
 
 export default function Home() {
+  const user = checkUser();
   return (
     <div className="h-full">
       <Navbar />
